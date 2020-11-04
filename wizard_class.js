12e5b35d3db4f038af17bb5dafe70eafdf2983c0
Dimitri McDaniel <devebeme@gmail.com>
2020-11-04 10:45:17 -0500

    Update wizard_class.js

--- wizard_class.js
+++ wizard_class.js
@@ -1,4 +1,4 @@
-class Wizard {
+class Player {
     constructor(name,main_element,second_element,home_town){
         this.name = name;
         this.main_element = main_element;
@@ -10,5 +10,21 @@ class Wizard {
     }
 }
 
+// when we extend a class we must call constructor to the newly constructed class
+class Wizard extends Player {
+    constructor(name,main_element,second_element,home_town){
+        // when inheriting the properties from the inherited class super helps us gain the context to our class
+        super(name,main_element,second_element,home_town);
+    }
+    play() {
+        console.log(`WOOOOO i'm a ${this.main_element}/${this.second_element} based wizard!`);
+    }
+} 
+
 const wizard1 = new Wizard('xanthu', 'lighting', 'water', 'lumbridge');
+const wizard2 = new Wizard('zamorak', 'Fire', 'earth', 'falador');
 wizard1.introduce();
+wizard2.introduce();
+wizard1.play();
+wizard2.play();
+
